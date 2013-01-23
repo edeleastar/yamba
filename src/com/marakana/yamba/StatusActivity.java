33e3b298102e011854992f70444ef5c9d7a9af19
Eamonn de Leastar <edeleastar@gmail.com>
2013-01-23 17:00:07 +0000

    Chapter 7: Preferences Activity

--- src/com/marakana/yamba/StatusActivity.java
+++ src/com/marakana/yamba/StatusActivity.java
@@ -3,12 +3,16 @@ package com.marakana.yamba;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -34,10 +38,10 @@ public class StatusActivity extends Activity implements OnClickListener, TextWat
     updateButton = (Button) findViewById(R.id.buttonUpdate);
     updateButton.setOnClickListener(this);
 
-    textCount = (TextView) findViewById(R.id.textCount); 
-    textCount.setText(Integer.toString(140)); 
-    textCount.setTextColor(Color.GREEN); 
-    editText.addTextChangedListener(this); 
+    textCount = (TextView) findViewById(R.id.textCount);
+    textCount.setText(Integer.toString(140));
+    textCount.setTextColor(Color.GREEN);
+    editText.addTextChangedListener(this);
 
     twitter = new Twitter("student", "password");
     twitter.setAPIRootUrl("http://yamba.marakana.com/api");
@@ -48,6 +52,26 @@ public class StatusActivity extends Activity implements OnClickListener, TextWat
     String status = editText.getText().toString();
     new PostToTwitter().execute(status);
     Log.d(TAG, "onClicked");
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    switch (item.getItemId())
+    { 
+    case R.id.itemPrefs:
+      startActivity(new Intent(this, PrefsActivity.class)); 
+      break;
+    }
+    return true; 
   }
 
   class PostToTwitter extends AsyncTask<String, Integer, String>
@@ -82,19 +106,19 @@ public class StatusActivity extends Activity implements OnClickListener, TextWat
   }
 
   public void afterTextChanged(Editable statusText)
-  { 
-    int count = 140 - statusText.length(); 
+  {
+    int count = 140 - statusText.length();
     textCount.setText(Integer.toString(count));
-    textCount.setTextColor(Color.GREEN); 
+    textCount.setTextColor(Color.GREEN);
     if (count < 10) textCount.setTextColor(Color.YELLOW);
     if (count < 0) textCount.setTextColor(Color.RED);
   }
 
   public void beforeTextChanged(CharSequence s, int start, int count, int after)
-  { 
+  {
   }
 
   public void onTextChanged(CharSequence s, int start, int before, int count)
-  { 
+  {
   }
 }
