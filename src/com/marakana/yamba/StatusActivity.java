8da3d0bce708485781321b2ed3368e077e9648e6
Eamonn de Leastar <edeleastar@gmail.com>
2013-01-23 16:36:05 +0000

    Chapter 6: Other UI Events

--- src/com/marakana/yamba/StatusActivity.java
+++ src/com/marakana/yamba/StatusActivity.java
@@ -3,21 +3,26 @@ package com.marakana.yamba;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class StatusActivity extends Activity implements OnClickListener
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher
 {
   private static final String TAG = "StatusActivity";
   EditText editText;
   Button updateButton;
   Twitter twitter;
+  TextView textCount;
 
   @Override
   public void onCreate(Bundle savedInstanceState)
@@ -29,15 +34,27 @@ public class StatusActivity extends Activity implements OnClickListener
     updateButton = (Button) findViewById(R.id.buttonUpdate);
     updateButton.setOnClickListener(this);
 
+    textCount = (TextView) findViewById(R.id.textCount); 
+    textCount.setText(Integer.toString(140)); 
+    textCount.setTextColor(Color.GREEN); 
+    editText.addTextChangedListener(this); 
+
     twitter = new Twitter("student", "password");
     twitter.setAPIRootUrl("http://yamba.marakana.com/api");
   }
 
+  public void onClick(View v)
+  {
+    String status = editText.getText().toString();
+    new PostToTwitter().execute(status);
+    Log.d(TAG, "onClicked");
+  }
+
   class PostToTwitter extends AsyncTask<String, Integer, String>
-  { 
+  {
     @Override
     protected String doInBackground(String... statuses)
-    { 
+    {
       try
       {
         Twitter.Status status = twitter.updateStatus(statuses[0]);
@@ -53,21 +70,31 @@ public class StatusActivity extends Activity implements OnClickListener
 
     @Override
     protected void onProgressUpdate(Integer... values)
-    { 
+    {
       super.onProgressUpdate(values);
     }
 
     @Override
     protected void onPostExecute(String result)
-    { 
+    {
       Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
     }
   }
 
-  public void onClick(View v)
-  {
-    String status = editText.getText().toString();
-    new PostToTwitter().execute(status);
-    Log.d(TAG, "onClicked");
+  public void afterTextChanged(Editable statusText)
+  { 
+    int count = 140 - statusText.length(); 
+    textCount.setText(Integer.toString(count));
+    textCount.setTextColor(Color.GREEN); 
+    if (count < 10) textCount.setTextColor(Color.YELLOW);
+    if (count < 0) textCount.setTextColor(Color.RED);
+  }
+
+  public void beforeTextChanged(CharSequence s, int start, int count, int after)
+  { 
+  }
+
+  public void onTextChanged(CharSequence s, int start, int before, int count)
+  { 
   }
 }
