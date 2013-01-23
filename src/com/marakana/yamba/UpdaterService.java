e96489ba0d7bf933a4098f774987449dbe27fe2f
Eamonn de Leastar <edeleastar@gmail.com>
2013-01-23 18:50:40 +0000

    Chapter 8: Looping in the Service

--- src/com/marakana/yamba/UpdaterService.java
+++ src/com/marakana/yamba/UpdaterService.java
@@ -7,33 +7,77 @@ import android.util.Log;
 
 public class UpdaterService extends Service
 {
-  static final String TAG = "UpdaterService";
+  private static final String TAG = "UpdaterService";
+
+  static final int DELAY = 60000; 
+  private boolean runFlag = false;
+  private Updater updater;
 
   @Override
   public IBinder onBind(Intent intent)
-  { 
+  {
     return null;
   }
 
   @Override
   public void onCreate()
-  { 
+  {
     super.onCreate();
+
+    this.updater = new Updater();
+
     Log.d(TAG, "onCreated");
   }
 
   @Override
   public int onStartCommand(Intent intent, int flags, int startId)
-  { 
+  {
     super.onStartCommand(intent, flags, startId);
+
+    this.runFlag = true; 
+    this.updater.start();
+
     Log.d(TAG, "onStarted");
     return START_STICKY;
   }
 
   @Override
   public void onDestroy()
-  { // <5>
+  {
     super.onDestroy();
+
+    this.runFlag = false; 
+    this.updater.interrupt(); 
+    this.updater = null;
+
     Log.d(TAG, "onDestroyed");
   }
+
+
+  private class Updater extends Thread
+  { 
+    public Updater()
+    {
+      super("UpdaterService-Updater"); 
+    }
+
+    @Override
+    public void run()
+    { 
+      UpdaterService updaterService = UpdaterService.this; 
+      while (updaterService.runFlag)
+      {
+        Log.d(TAG, "Updater running");
+        try
+        {
+          Log.d(TAG, "Updater ran");
+          Thread.sleep(DELAY); 
+        }
+        catch (InterruptedException e)
+        { 
+          updaterService.runFlag = false;
+        }
+      }
+    }
+  } 
 }
